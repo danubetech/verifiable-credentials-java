f056a70e642ec64c8baf61a0f8ca9e0d4fbd6400
Viacheslav Herasymov <viacheslav.herasymov@sicpa.com>
2023-09-01 14:06:15 +0200

    chore: add credential status test vc compilance

--- src/main/java/com/danubetech/verifiablecredentials/validation/Validation.java
+++ src/main/java/com/danubetech/verifiablecredentials/validation/Validation.java
@@ -54,6 +54,8 @@ public class Validation {
         validateRun(() -> { verifiableCredential.getExpirationDate(); }, "Bad 'expirationDate'.");
         validateRun(() -> { verifiableCredential.getCredentialSubject(); }, "Bad 'credentialSubject'.");
         validateRun(() -> { validateTrue(verifiableCredential.getCredentialSubject() != null); }, "Bad or missing 'credentialSubject'.");
+        validateRun(() -> { if (verifiableCredential.getCredentialStatus() != null) validateTrue(verifiableCredential.getCredentialStatus().getId() != null); }, "'credentialStatus' must contain 'id'.");
+        validateRun(() -> { if (verifiableCredential.getCredentialStatus() != null) validateTrue(verifiableCredential.getCredentialStatus().getType() != null); }, "'credentialStatus' must contain 'type'.np");
     }
 
     public static void validate(VerifiablePresentation verifiablePresentation) throws IllegalStateException {
